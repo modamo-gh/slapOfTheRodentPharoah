82e71d64080bd61dce8e0620c049154f52f6f6b2
Bryan <morganthemosaic@gmail.com>
2024-08-22 20:57:17 -0400

    Create Card class

---
+++ src/models/Card.ts
@@ -0,0 +1,9 @@
+export default class Card {
+	suit: string;
+	value: string;
+
+	constructor(suit: string, value: string) {
+		this.suit = suit;
+		this.value = value;
+	}
+}
---
+++ test.ts
@@ -0,0 +1,5 @@
+import Card from "./src/models/Card";
+
+const card = new Card("Hearts", "3");
+
+console.log(card);
