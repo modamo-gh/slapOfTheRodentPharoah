312325c9046ce5c7ea520a1f6fba6cbb3b210ea3
Bryan <morganthemosaic@gmail.com>
2024-08-23 08:51:32 -0400

    Implement deal and shuffle methods

--- src/models/Deck.ts
+++ src/models/Deck.ts
@@ -1,18 +1,62 @@
 import Card from "./Card";
 
-export default class Deck{
-    deck: Card[];
+export default class Deck {
+	deck: Card[];
 
-    constructor(){
-        this.deck = [];
+	constructor() {
+		this.deck = [];
 
-        const suits = ["Club", "Diamond", "Heart", "Spade"];
-        const values = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"];
+		const suits = ["Clubs", "Diamonds", "Hearts", "Spades"];
+		const values = [
+			"2",
+			"3",
+			"4",
+			"5",
+			"6",
+			"7",
+			"8",
+			"9",
+			"10",
+			"J",
+			"Q",
+			"K",
+			"A"
+		];
 
-        for(const suit of suits){
-            for(const value of values){
-                this.deck.push(new Card(suit, value));
-            }
-        }
-    }
+		for (const suit of suits) {
+			for (const value of values) {
+				this.deck.push(new Card(suit, value));
+			}
+		}
+	}
+
+	deal = (numberOfPlayers: number): Card[][] => {
+		const numberOfCardsPerPlayers = Math.floor(
+			this.deck.length / numberOfPlayers
+		);
+
+		const hands: Card[][] = Array.from(
+			{ length: numberOfPlayers },
+			() => []
+		);
+
+		while (hands[numberOfPlayers - 1].length < numberOfCardsPerPlayers) {
+			for (let i = 0; i < numberOfPlayers; i++) {
+				const card = this.deck.pop();
+
+				if (card) {
+					hands[i].push(card);
+				}
+			}
+		}
+
+		return hands;
+	};
+
+	shuffle = () => {
+		for (let i = this.deck.length - 1; i >= 0; i--) {
+			const j = Math.floor(Math.random() * (i + 1));
+			[this.deck[i], this.deck[j]] = [this.deck[j], this.deck[i]];
+		}
+	};
 }
--- test.ts
+++
@@ -1,5 +0,0 @@
-import Card from "./src/models/Card";
-
-const card = new Card("Hearts", "3");
-
-console.log(card);
